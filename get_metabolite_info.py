561e117f0175d6ba0b6cb9216b81bc38632aa2c2
tim-peterson <petersontimr@gmail.com>
2017-01-19 22:26:48 -0600

    pubchem

---
+++ get_metabolite_info.py
@@ -0,0 +1,34 @@
+import requests
+import csv
+import time
+
+with open('20160109RJ755 HML Compounds available.csv','r') as csvinput:
+	with open('20160109RJ755 HML Compounds available_v2.csv', 'w') as csvoutput:
+		writer = csv.writer(csvoutput, lineterminator='\n')
+		reader = csv.reader(csvinput)
+
+		all = []
+		row = next(reader)
+		row.append('CID')
+		all.append(row)
+
+		for row in reader:
+			print row[1]
+			response = requests.get('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/' + row[1] + '/cids/TXT')
+			print response.text
+			#quit()
+			row.append(response.text.strip())
+
+			#row.append(row[0])
+			all.append(row)
+
+			#time.sleep(0.1)
+
+		writer.writerows(all)
+
+
+
+
+
+
+
